ad39db2cbf7bd303ddaeb2cb450e2b12a51d26e3
foxyoperator <dundipokom8@gmail.com>
2026-06-09 20:30:42 +0200

    app.py

--- app.py
+++ app.py
@@ -5,25 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """
-    <html>
-        <head>
-            <title>Foxy_Company_Cloud</title>
-            <style>
-                body { background-color: #121212; color: #ff6600; font-family: sans-serif; text-align: center; padding-top: 50px; }
-                h1 { font-size: 50px; }
-                p { color: #ffffff; font-size: 20px; }
-            </style>
-        </head>
-        <body>
-            <h1>🦊 FOXY_COMPANY SYSTEM 🦊</h1>
-            <p>A felhő alapú rendszer sikeresen elindult és online van!</p>
-            <p>RANG: OPERÁTOR</p>
-        </body>
-    </html>
-    """
+    return "Foxy_Company_Cloud is Running!"
 
 if __name__ == '__main__':
-    # A Render automatikusan beállítja a PORT-ot, ha nincs, akkor 25565-ön fut
     port = int(os.environ.get('PORT', 25565))
     app.run(host='0.0.0.0', port=port)
